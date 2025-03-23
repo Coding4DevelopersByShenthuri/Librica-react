976c2974231a4be3d7ee6e4acca1451944621f64
Shenthuri Maran <168589614+Coding4DevelopersByShenthuri@users.noreply.github.com>
2025-03-23 15:59:57 +0530

    ok

--- backend/app.js
+++ backend/app.js
@@ -6,12 +6,16 @@ const app = express();
 app.use(express.json());
 const Books = require("./models/book");
 const Favourite = require("./routes/favourite");
+const Cart = require("./routes/cart");
+const Order = require("./routes/order");
 
 
 // routes
 app.use("/api/v1", User);
 app.use("/api/v1", Books);
 app.use("/api/v1", Favourite);
+app.use("/api/v1", Cart);
+app.use("/api/v1", Order);
 
 // Creating port
 const PORT = process.env.PORT || 5000;
--- backend/routes/favourite.js
+++ backend/routes/favourite.js
@@ -7,7 +7,7 @@ router.put("/add-book-to-favourite", authenticateToken, async (req, res) => {
     try {
         const { bookid, id } = req.headers;
         const userData = await User.findById(id);
-        const isBookFavourite = userData.favourite.includes(bookid);
+        const isBookFavourite = userData.favourites.includes(bookid);
         if (isBookFavourite) {
             return res.status(200).json({ message: "Book already added to favourite" });
         }
@@ -15,6 +15,37 @@ router.put("/add-book-to-favourite", authenticateToken, async (req, res) => {
         return res.status(200).json({ message: "Book added to favourite successfully" });
     } catch (error) {
         res.status(500).json({ message: "Internal Server Error" });
+    }
+});
+
+// add book to favourite
+router.put("/remove-book-from-favourite", authenticateToken, async (req, res) => {
+    try {
+        const { bookid, id } = req.headers;
+        const userData = await User.findById(id);
+        const isBookFavourite = userData.favourites.includes(bookid);
+        if (isBookFavourite) {
+            await User.findByIdAndUpdate(id, { $pull: { favourite: bookid } });
+        }
+        return res.status(200).json({ message: "Book removed from favourite successfully" });
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+});
+
+// get favourite books of a particular user
+router.get("/get-favourite-books", authenticateToken, async (req, res) => {
+    try {
+        const { id } = req.headers;
+        const userData = await User.findById(id).populate("favourites");
+        const favouriteBooks = userData.favourites;
+        return res.json({
+            status: "Success",
+            data: favouriteBooks,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "An Error Occurred" });
     }
 });
 module.exports = router;
